998a3088f77bba740b4e0690dda18cfde0be8234
dvs23 <dvs23@dvs-board.de>
2018-06-09 18:08:45 +0200

    Visualization pretty nice now :)

--- visualization/index.js
+++ visualization/index.js
@@ -3,10 +3,54 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var path = require('path');
 var mysql = require('mysql');
+var bodyParser = require("body-parser");
 var app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.use(serveStatic(path.join(__dirname, 'static')));
 
-//app.post
+app.post("/getData", function(req, res){
+    console.log("Hallo", req.body.emotion);
+    if(req.body.emotion === "anger"){
+        res.send({
+            tweets: [
+                {
+                    name: "Massachusetts",
+                    anger: 0.8
+                },
+                {
+                    name: "Massachusetts",
+                    anger: 0.5
+                },
+                {
+                    name: "Massachusetts",
+                    anger: 0.7
+                }
+            ]
+        });
+    }
+    else if(req.body.emotion === "disgust"){
+        res.send({
+            tweets: [
+                {
+                    name: "Utah",
+                    anger: 0.8
+                },
+                {
+                    name: "Massachusetts",
+                    anger: 0.3
+                },
+                {
+                    name: "Kentucky",
+                    anger: 0.7
+                }
+            ]
+        });
+    }
+});
 
 app.listen(8080);
