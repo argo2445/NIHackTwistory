17785769cc0ce809bfb5e49e1794582da3f3bf79
dvs23 <dvs23@dvs-board.de>
2018-06-09 13:06:51 +0200

    Working static serving in visualizer :)

---
+++ visualization/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var express = require('express');
+var serveStatic = require('serve-static');
+var path = require('path');
+var mysql = require('mysql');
+var app = express();
+
+app.use(serveStatic(path.join(__dirname, 'static')));
+app.listen(8080);
